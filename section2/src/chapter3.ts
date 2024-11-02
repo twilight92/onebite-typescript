594517a16ba4f2e921ade52bf1bbbfe26ad3ba69
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-02 12:42:52 +0900

    객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,18 @@
+// object
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "이정환",
+};
+
+user = {
+  name: "홍길동",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "asdfasdf",
+};
