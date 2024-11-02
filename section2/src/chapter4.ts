d2a9f515645cd9fbf207462ebbc0fb5ff0b00bf7
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-02 12:46:56 +0900

    공통적으로 적용되어야하는 타입은 타입 별칭 이용

---
+++ section2/src/chapter4.ts
@@ -0,0 +1,27 @@
+// 타입 별칭
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+
+let user1: User = {
+  id: 1,
+  name: "이정환",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+
+let user2: User = {
+  id: 1,
+  name: "홍길동",
+  nickname: "dong",
+  birth: "1997.01.07",
+  bio: "hi",
+  location: "서울",
+};
