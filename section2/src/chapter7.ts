f6151a8da53da6f0c6d4ff37dadd25dfa546ac6d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-03 14:28:10 +0900

    never 타입

--- section2/src/chapter7.ts
+++ section2/src/chapter7.ts
@@ -10,3 +10,25 @@ function func1(): string {
 function func2(): void {
   console.log("hello");
 }
+
+// never
+// never -> 존재하지 않는
+// 불가능한 타입
+
+function func3(): never {
+  while (true) {}
+}
+
+function func4(): never {
+  throw new Error();
+}
+
+let anyVar: any;
+
+let a: never;
+a = 1;
+a = {};
+a = "";
+a = undefined;
+a = null;
+a = anyVar;
