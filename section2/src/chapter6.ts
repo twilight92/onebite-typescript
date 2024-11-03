5e6e89effaa1c55549bfc6c5f4d16ddb33cd72f9
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-03 14:01:44 +0900

    Unknown 타입

--- section2/src/chapter6.ts
+++ section2/src/chapter6.ts
@@ -4,3 +4,14 @@ let anyVar: any = 10;
 
 let num: number = 10;
 num = anyVar;
+
+// unknown
+let unknownVar: unknown;
+
+unknownVar = "";
+unknownVar = 1;
+unknownVar = () => {};
+
+if (typeof unknownVar === "number") {
+  num = unknownVar;
+}
