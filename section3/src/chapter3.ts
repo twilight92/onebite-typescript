1fe4cc923b234e64239399c9ad82c84b05e5a2fd
twilight92 <tnwjd2050@naver.com>
2024-11-04 07:54:07 +0900

    객체 타입의 호환성

---
+++ section3/src/chapter3.ts
@@ -0,0 +1,72 @@
+/**
+ * 객체 타입간의 호환성
+ * -> 어떤 객체타입을 다른 객체타입으로 취급해도 괜찮은가?
+ */
+
+type Animal = {
+  name: string;
+  color: string;
+}
+
+type Dog = {
+  name: string;
+  color: string;
+  breed: string;
+}
+
+let animal: Animal = {
+  name: "기린",
+  color: "yellow"
+}
+
+let dog: Dog = {
+  name: "돌돌이",
+  color: "brown",
+  breed: "진도"
+}
+
+animal = dog;
+// dog = animal;
+
+type ProgrammingBook = {
+  name: string;
+  price: number;
+  skill: string;
+}
+
+let book: Book;
+let programmingBook: ProgrammingBook = {
+  name: "한 입 크기도 잘라먹는 리액트",
+  price: 33000,
+  skill: "reactjs"
+}
+
+book = programmingBook;
+// programmingBook = book;
+
+/**
+ * 초과 프로퍼티 검사
+ */
+
+type Book = {
+  name: string;
+  price: number;
+}
+
+let book2: Book = {
+  name: "한 입 크기도 잘라먹는 리액트",
+  price: 33000,
+  // skill: "reactjs"
+}
+
+let book3: Book = programmingBook;
+
+function func(bool: Book) { }
+
+func({
+  name: "한 입 크기도 잘라먹는 리액트",
+  price: 33000,
+  // skill: "reactjs"
+})
+
+func(programmingBook);
